2fbb5c2ade95877a68ca98404a1b01f2b81d6034
Andre @ code1 <agbowlin@gmail.com>
2020-11-28 14:46:17 -0600

    updated build

--- build/custom/910-release-version.js
+++ build/custom/910-release-version.js
@@ -3,15 +3,21 @@
 
 Finalizes a library project for release and establishes a new version.
 
-## Finalize Project for Release
+# Procedure
 
 - Loads the package.json file to obtain current version number.
-- Runs tests and store output: `npx mocha -u bdd tests/*.js --timeout 0 --slow 10`
-- Do final commit: `git commit -a -m "Finalization for vX.Y.Z"`
-- Do final push: `git push origin`
+
+## Finalize Project for Release
+
+- Runs tests and store output in `docs/testing-output.md`: `npx mocha -u bdd tests/*.js --timeout 0 --slow 10`
+- Do webpack: `bash build/webpack/010-webpack.sh`
+- Do final staging: `git add .`
+- Do final commit: `git commit -m "Finalization for vX.Y.Z"`
+- Do final push: `git push origin master`
 - Create git version tag: `git tag -a vX.Y.Z -m "Version vX.Y.Z"`
 - Push git version tag: `git push origin vX.Y.Z`
 - Create new npm version: `npm publish . --access public`
+- Update S3 docs: `bash build/s3/810-s3-sync-docs.sh`
 
 ## Establish a New Version
 
@@ -22,11 +28,20 @@ Finalizes a library project for release and establishes a new version.
 	- `readme.md` (in format: `(vX.Y.Z)`)
 	- `docs/_coverpage.md` (in format: `(vX.Y.Z)`)
 	- `docs/guides/readme.md` (in format: `(vX.Y.Z)`)
+
+## Cleanup New Version
+
+- Do initial staging: `git add .`
+- Do initial commit: `git commit -m "Initialization for vX.Y.Z"`
+- Do initial push: `git push origin master`
+
 */
 
 const LIB_FS = require( 'fs' );
 const LIB_PATH = require( 'path' );
 const LIB_CHILD_PROCESS = require( 'child_process' );
+
+const NPM = true;
 
 
 //---------------------------------------------------------------------
@@ -86,9 +101,10 @@ function replace_text( Text, Search, Replace )
 //---------------------------------------------------------------------
 ( async function ()
 {
+	console.log( `[910-release-version] starting` );
 	console.log( `Running in: ${process.cwd()}` );
 
-	// Load package.json
+	// - Loads the package.json file to obtain current version number.
 	let path = LIB_PATH.join( process.cwd(), 'package.json' );
 	let PACKAGE = require( path );
 	console.log( `Loaded package.json` );
@@ -99,8 +115,12 @@ function replace_text( Text, Search, Replace )
 	//	Finalize Project for Release
 	//---------------------------------------------------------------------
 
-	// Update docs/testing-output.md
-	console.log( 'Updating docs/testing-output.md' );
+	// - Do webpack: `bash build/webpack/010-webpack.sh`
+	console.log( 'Do webpack' );
+	await execute_command( `bash build/webpack/010-webpack.sh` );
+
+	// - Runs tests and store output in docs/testing-output.md: `npx mocha -u bdd tests/*.js --timeout 0 --slow 10`
+	console.log( 'Runs tests and store output in docs/testing-output.md' );
 	{
 		let result = await execute_command( `npx mocha -u bdd tests/*.js --timeout 0 --slow 10` );
 		path = LIB_PATH.join( process.cwd(), 'docs', 'testing-output.md' );
@@ -112,25 +132,38 @@ function replace_text( Text, Search, Replace )
 		);
 	}
 
-	return;
+	// - Do final staging: `git add .`
+	console.log( 'Do final staging' );
+	await execute_command( `git add .` );
 
-	// Set git version tag.
-	console.log( 'Setting git version tag' );
+	// - Do final commit: `git commit -m "Finalization for vX.Y.Z"`
+	console.log( 'Do final commit' );
+	await execute_command( `git commit -m "Finalization for v${PACKAGE.version}"` );
+
+	// - Do final push: `git push origin master`
+	console.log( 'Do final push' );
+	await execute_command( `git push origin master` );
+
+	// - Create git version tag: `git tag -a vX.Y.Z -m "Version vX.Y.Z"`
+	console.log( 'Create git version tag' );
 	await execute_command( `git tag -a v${PACKAGE.version} -m "Version ${PACKAGE.version}"` );
 
-	// Push git version tag.
-	console.log( 'Pushing git version tag' );
+	// - Push git version tag: `git push origin vX.Y.Z`
+	console.log( 'Push git version tag' );
 	await execute_command( `git push origin v${PACKAGE.version}` );
 
-	// Update npm with new release.
-	console.log( 'Updating npm with new release' );
-	await execute_command( `npm publish . --access public` );
+	if ( NPM )
+	{
+		// - Create new npm version: `npm publish . --access public`
+		console.log( 'Create new npm version' );
+		await execute_command( `npm publish . --access public` );
+	}
 
 	//---------------------------------------------------------------------
 	//	Establish a New Version
 	//---------------------------------------------------------------------
 
-	// Increment the version number.
+	// - Increments the minor portion of the version number (e.g. 0.0.1 -> 0.0.2).
 	let prev_version = PACKAGE.version;
 	{
 		let parts = PACKAGE.version.split( '.' );
@@ -138,7 +171,7 @@ function replace_text( Text, Search, Replace )
 		PACKAGE.version = parts.join( '.' );
 	}
 
-	// Update files with new version.
+	// - Search/Replace the old version number with the new version in source files:
 	let doc = null;
 
 	// Update package.json
@@ -169,5 +202,23 @@ function replace_text( Text, Search, Replace )
 	doc = replace_text( doc, `(v${prev_version})`, `(v${PACKAGE.version})` );
 	LIB_FS.writeFileSync( path, doc );
 
+	//---------------------------------------------------------------------
+	//	Cleanup New Version
+	//---------------------------------------------------------------------
+
+	// - Do initial staging: `git add .`
+	console.log( 'Do initial staging' );
+	await execute_command( `git add .` );
+
+	// - Do initial commit: `git commit -m "Initialization for vX.Y.Z"`
+	console.log( 'Do initial commit' );
+	await execute_command( `git commit -m "Initialization for v${PACKAGE.version}"` );
+
+	// - Do final push: `git push origin master`
+	console.log( 'Do final push' );
+	await execute_command( `git push origin master` );
+
+
+	console.log( `[910-release-version] finished` );
 	return;
 } )();
